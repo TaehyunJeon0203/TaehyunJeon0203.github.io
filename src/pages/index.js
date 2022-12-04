b3532da5b28ef8d94b7b665abb6494369c85a583
TaehyunJeon <jeontaehyun0203@gmail.com>
2022-12-04 13:58:08 +0000

    reset

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,65 +1,7 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
-
-const links = [
-  {
-    text: "test_1",
-    url: "page-2.js",
-    description:
-      "Test Post",
-  }
-]
-
-const moreLinks = [
-  { text: "Instagram", url: "https://www.instagram.com/jeontaehyun0203/" },
-  { text: "Github", url: "https://github.com/TaehyunJeon0203" },
-]
-
-const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
-
-const IndexPage = () => (
-  <Layout>
-    <div className={styles.textCenter}>
-      <h1>
-        Welcome To <b>Taehyun's blog</b>
-      </h1>
-    </div>
-    <ul className={styles.list}>
-      {links.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-            >
-            {link.text} ↗
-          </a>
-          <p className={styles.listItemDescription}>{link.description}</p>
-        </li>
-      ))}
-    </ul>
-    <div>
-      <hr></hr>
-      <p><b>Contact & Channel</b></p>
-      {moreLinks.map((link, i) => (
-        <React.Fragment key={link.url}>
-          <a href={`${link.url}${utmParameters}`}>{link.text}</a>
-          {i !== moreLinks.length - 1 && <> · </>}
-        </React.Fragment>
-      ))}
-    </div>
-  </Layout>
+const Index = () => (
+  <p></p>
 )
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
-export const Head = () => <Seo title="Home" />
-
-export default IndexPage
+export default Index
