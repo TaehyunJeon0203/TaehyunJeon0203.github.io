451899e4ed94ac9f45716267cd0aa9270201ef05
TaehyunJeon <jeontaehyun0203@gmail.com>
2023-12-17 23:56:14 +0900

    design: 헤더 디자인 변경 중(svg 정상적으로 렌더링되지 않는 이슈 있음)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
+import Logo from './Logo';
 
-const Header = ({ title }) => (
-  <header className="global-header">
-    <nav className="navbar">
-      <Link to="/" className="navbar-brand">
-        {title}
-      </Link>
-      {/* 여기에 추가적인 메뉴나 로고 등을 넣어주세요 */}
-    </nav>
-  </header>
-)
+const Header = ({ title }) => {
+    return (
+        <header className="global-header">
+            <nav className="navbar">
+            <Link to="/" className="navbar-brand">
+                <Logo width="100" height="100" className="custom-logo-class"/>
+            </Link>
+            {/* 여기에 추가적인 메뉴나 로고 등을 넣어주세요 */}
+            </nav>
+        </header>
+    );
+};
 
 export default Header
