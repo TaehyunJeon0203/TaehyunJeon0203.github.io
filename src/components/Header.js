ba866405ee6f2b485aa5b602e18e2a1f777bd0fb
TaehyunJeon <jeontaehyun0203@gmail.com>
2023-12-24 17:40:28 +0900

    header-menuIcon 추가 (기능구현 x)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
-import React from "react";
-import { Link } from "gatsby";
-import Logo from './Logo';
+import React from "react"
+import { Link } from "gatsby"
+import { Menu } from "react-feather"
+import Logo from "./Logo"
 
-const Header = ({ title }) => {
-    return (
-        <header className="global-header">
-            <Link to="/">
-                <Logo width="90" height="" />
-            </Link>
-        </header>
-    );
-};
+const Header = () => {
+  return (
+    <header className="global-header">
+      <Link to="/" style={{ height: "26px" }}>
+        <Logo width="90" height="" />
+      </Link>
+      <Menu size={25} />
+    </header>
+  )
+}
 
 export default Header
