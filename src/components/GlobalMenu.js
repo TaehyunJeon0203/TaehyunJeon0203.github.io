69ec09930c9316e4faebc427679f1e157ec37bb7
TaehyunJeon <jeontaehyun0203@gmail.com>
2024-05-14 01:32:11 +0900

    Feat: 메뉴 구현 방식 react-modal로 변경 및 애니메이션 추가

--- src/components/GlobalMenu.js
+++ src/components/GlobalMenu.js
@@ -1,28 +1,35 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react"
+import Modal from "react-modal"
 import "../style/GlobalMenu.css"
 import GlobalMenuHeader from "./GlobalMenuHeader"
 import GlobalMenuFooter from "./GlobalMenuFooter"
 import GlobalMenuItem from "./GlobalMenuItem"
 
 const GlobalMenu = ({ isOpen, toggleMenu }) => {
-  const handleOutsideClick = e => {
-    if (isOpen && !e.target.closest(".menu")) {
-      toggleMenu() // 메뉴 닫기
-    }
+  const [isClosing, setIsClosing] = useState(false)
+
+  const handleCloseModal = () => {
+    setIsClosing(true) // 모달이 닫히는 중임을 설정합니다.
+    setTimeout(() => {
+      toggleMenu() // 모달을 닫습니다.
+      setIsClosing(false) // 모달이 닫혔음을 설정합니다.
+    }, 500) // setTimeout으로 모달이 완전히 닫히는 데 0.3초를 설정합니다.
   }
 
   return (
-    <div
-      className={`menu-container ${isOpen ? "open" : ""}`}
-      onClick={handleOutsideClick}
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={handleCloseModal}
+      contentLabel="Global Menu"
+      className={`menu-modal ${isOpen && !isClosing ? "slideIn" : "slideOut"}`}
+      overlayClassName="menu-overlay"
     >
       <div className="menu">
         <GlobalMenuHeader />
         <GlobalMenuItem />
         <GlobalMenuFooter />
       </div>
-    </div>
+    </Modal>
   )
 }
 
