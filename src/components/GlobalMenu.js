1c4da42ff450ad5fda5aafc19c857c65c2ed8755
TaehyunJeon <jeontaehyun0203@gmail.com>
2024-09-16 23:01:55 +0900

    Docs: Grav 개발기록 작성

--- src/components/GlobalMenu.js
+++ src/components/GlobalMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Modal from "react-modal"
 import "../style/GlobalMenu.css"
 import GlobalMenuHeader from "./GlobalMenuHeader"
@@ -8,12 +8,20 @@ import GlobalMenuItem from "./GlobalMenuItem"
 const GlobalMenu = ({ isOpen, toggleMenu }) => {
   const [isClosing, setIsClosing] = useState(false)
 
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("menu-open") // 메뉴가 열릴 때 body에 menu-open 클래스 추가
+    } else {
+      document.body.classList.remove("menu-open") // 메뉴가 닫힐 때 body에서 menu-open 클래스 제거
+    }
+  }, [isOpen])
+
   const handleCloseModal = () => {
     setIsClosing(true) // 모달이 닫히는 중임을 설정합니다.
     setTimeout(() => {
       toggleMenu() // 모달을 닫습니다.
       setIsClosing(false) // 모달이 닫혔음을 설정합니다.
-    }, 500) // setTimeout으로 모달이 완전히 닫히는 데 0.3초를 설정합니다.
+    }, 500) // setTimeout으로 모달이 완전히 닫히는 데 0.5초를 설정합니다.
   }
 
   return (
