9f6ca4969024f9a36596a89480e2f3ed77b04ad5
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-06-27 15:55:44 +0900

    Chore: 구글 검색엔진 등록 최적화

--- src/components/seo.js
+++ src/components/seo.js
@@ -17,6 +17,7 @@ const Seo = ({ description, title, children }) => {
           siteMetadata {
             title
             description
+            siteUrl
             social {
               twitter
             }
@@ -28,26 +29,55 @@ const Seo = ({ description, title, children }) => {
 
   const metaDescription = description || site.siteMetadata.description
   const defaultTitle = site.siteMetadata?.title
+  const siteUrl = site.siteMetadata?.siteUrl
 
   return (
     <>
       <title>{title ? `${title} | ${defaultTitle}` : defaultTitle}</title>
       <meta name="description" content={metaDescription} />
+
+      {/* Google Search Console Verification */}
+      <meta
+        name="google-site-verification"
+        content="OkRBNykCrS_DHs7gaT-rC15Z2kYSTHIaNJJmbqHQmVg"
+      />
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={siteUrl} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary" />
+      <meta property="og:site_name" content={defaultTitle} />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
       <meta
         name="twitter:creator"
         content={site.siteMetadata?.social?.twitter || ``}
       />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={metaDescription} />
+
+      {/* Additional SEO Meta Tags */}
+      <meta name="robots" content="index, follow" />
+      <meta name="googlebot" content="index, follow" />
+      <link rel="canonical" href={siteUrl} />
+
       <Helmet>
-        <meta
-          name="google-site-verification"
-          content="OkRBNykCrS_DHs7gaT-rC15Z2kYSTHIaNJJmbqHQmVg"
-        />
+        {/* Structured Data for Blog */}
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Blog",
+            name: defaultTitle,
+            description: metaDescription,
+            url: siteUrl,
+            publisher: {
+              "@type": "Person",
+              name: "TaehyunJeon",
+            },
+          })}
+        </script>
       </Helmet>
       {children}
     </>
