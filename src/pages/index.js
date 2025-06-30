d25a5bb9f0beafd1a64997c24f28e2188654a306
TaehyunJeon <jeontaehyun0203@gmail.com>
2025-06-30 17:39:09 +0900

    Design: 브라우저 탭 제목 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,7 +72,7 @@ const BlogIndex = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <Seo
-        title="개발자 전태현의 기술 및 일상 블로그"
+        title="TH blog"
         description="개발자 전태현의 개발, 일상, 여행 이야기를 공유합니다."
       />
       <Profile profile={profile} />
